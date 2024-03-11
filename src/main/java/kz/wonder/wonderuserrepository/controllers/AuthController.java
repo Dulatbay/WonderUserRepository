075a5e965f13315114514b3e5ba7135a4ef13bfc
Dulatbay <adulatbai@bk.ru>
2024-03-12 01:05:51 +0500

    add: registration logic

--- src/main/java/kz/wonder/wonderuserrepository/controllers/AuthController.java
+++ src/main/java/kz/wonder/wonderuserrepository/controllers/AuthController.java
@@ -1,10 +1,15 @@
 package kz.wonder.wonderuserrepository.controllers;
 
+import jakarta.validation.Valid;
+import kz.wonder.wonderuserrepository.dto.request.SellerRegistrationRequest;
 import kz.wonder.wonderuserrepository.dto.response.AuthResponse;
+import kz.wonder.wonderuserrepository.services.KeycloakService;
+import kz.wonder.wonderuserrepository.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+
+    private final KeycloakService keycloakService;
+    private final UserService userService;
+
     @PostMapping("/registration")
-    public ResponseEntity<AuthResponse> registration() {
-
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(AuthResponse.builder().build());
+    public ResponseEntity<AuthResponse> registration(@RequestBody @Valid SellerRegistrationRequest registrationRequestBody) {
+        // todo: create transactions
+        var userRepresentation = keycloakService.createUser(registrationRequestBody).toRepresentation();
+        registrationRequestBody.setKeycloakId(userRepresentation.getId());
+        userService.createUser(registrationRequestBody);
+        var authResponse = keycloakService.getAuthResponse(registrationRequestBody.getEmail(), registrationRequestBody.getPassword());
+        return ResponseEntity.status(HttpStatus.CREATED).body(authResponse);
     }
 }
