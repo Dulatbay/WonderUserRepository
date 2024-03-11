ba38252eaabf2a445fc7d64116431fc66c64088c
Dulatbay <adulatbai@bk.ru>
2024-03-12 01:05:17 +0500

    fix: remake SellerRegistrationRequest data

--- src/main/java/kz/wonder/wonderuserrepository/dto/request/SellerRegistrationRequest.java
+++ src/main/java/kz/wonder/wonderuserrepository/dto/request/SellerRegistrationRequest.java
@@ -1,9 +1,22 @@
 package kz.wonder.wonderuserrepository.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class SellerRegistrationRequest {
+    // todo: add validation
     private String firstName;
     private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+
+    // in kaspi
+    private String sellerName;
+    private String sellerId;
+    private String tokenKaspi;
+
+    @JsonIgnore
+    private String keycloakId;
 }
