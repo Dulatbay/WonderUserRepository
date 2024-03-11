266d817c8de8457a8e0af5759896f06f342965c5
Dulatbay <adulatbai@bk.ru>
2024-03-12 01:16:10 +0500

    add: ValueConstants Zone Id parameter

---
+++ src/main/java/kz/wonder/wonderuserrepository/constants/ValueConstants.java
@@ -0,0 +1,11 @@
+package kz.wonder.wonderuserrepository.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.ZoneId;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ValueConstants {
+    public static ZoneId ZONE_ID = ZoneId.of("UTC+05:00"); // Almaty, Kazakhstan
+}
