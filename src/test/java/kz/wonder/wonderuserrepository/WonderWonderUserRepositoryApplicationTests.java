731c1cbc25511286a93afd5a5009aa1661fcf60b
Dulatbay <adulatbai@bk.ru>
2024-03-27 17:20:19 +0500

    Refactor test structure to improve readability
    The test structure in WonderWonderUserRepositoryApplicationTests.java has been reorganized to improve readability. Several hard-coded strings were replaced with static final variables. The methods were rearranged for better logical organization and a new method, createHttpEntity, was introduced to avoid code duplication. The changes made make the test cases more concise and easier to understand.

--- src/test/java/kz/wonder/wonderuserrepository/WonderWonderUserRepositoryApplicationTests.java
+++ src/test/java/kz/wonder/wonderuserrepository/WonderWonderUserRepositoryApplicationTests.java
@@ -16,24 +16,26 @@ import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = WonderUserRepositoryApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class WonderWonderUserRepositoryApplicationTests {
-
 	@LocalServerPort
 	private int port;
-
 	@Autowired
 	private TestRestTemplate restTemplate;
 	private String accessToken;
+
+	private static final String SERVER_URL = "http://localhost:%d/api";
+	private static final String AUTH_REGISTRATION = "/auth/registration";
+	private static final String AUTH_LOGIN = "/auth/login";
+	private static final String BOX_TYPES = "/box-types";
 
 	public WonderWonderUserRepositoryApplicationTests() {
 		System.out.println("constructor");
@@ -49,7 +51,6 @@ class WonderWonderUserRepositoryApplicationTests {
 				.phoneNumber("test2")
 				.sellerId("test2")
 				.sellerName("test2")
-				.sellerId("test2")
 				.tokenKaspi("token2")
 				.build();
 	}
@@ -57,20 +58,6 @@ class WonderWonderUserRepositoryApplicationTests {
 	private static UserAuthRequest getAuthRequest() {
 		return new UserAuthRequest("tester@mail.ru", "test_tester");
 	}
-
-	private String getServerUrl() {
-		return String.format("http://localhost:%d/api", port);
-	}
-
-	@Test
-	public void whenCreateUser_thenStatus400() {
-		ResponseEntity<ErrorDto> res = restTemplate
-				.postForEntity(getServerUrl() + "/auth/registration", getRandomUser(), ErrorDto.class);
-
-		assertEquals(HttpStatus.BAD_REQUEST.value(), res.getStatusCode().value());
-		assertEquals("User exists with same username", Objects.requireNonNull(res.getBody()).getMessage());
-	}
-
 
 	@BeforeEach
 	public void initUser() {
@@ -83,35 +70,47 @@ class WonderWonderUserRepositoryApplicationTests {
 	}
 
 	@Test
-	void whenGetBoxTypes_thenStatus200() {
-		// Добавляем первый бокс
-		MultiValueMap<String, Object> firstBox = getBoxTypeCreateAsMultiValueMap("Box 1", "First Box Description");
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-		HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(firstBox, headers);
-		restTemplate.postForEntity(getServerUrl() + "/box-types", httpEntity, Void.class);
+	public void whenCreateUser_thenStatus400() {
+		ResponseEntity<ErrorDto> response = restTemplate
+				.postForEntity(getServerUrl() + AUTH_REGISTRATION, getRandomUser(), ErrorDto.class);
+		assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCode().value());
+		assertEquals("User exists with same username", Objects.requireNonNull(response.getBody()).getMessage());
+	}
 
+
+	@Test
+	void whenGetBoxTypes_thenStatus200() {
+		MultiValueMap<String, Object> firstBox = getBoxTypeCreateAsMultiValueMap("Box 1", "First Box Description");
+		HttpEntity<MultiValueMap<String, Object>> httpEntity = createHttpEntity(firstBox);
+		restTemplate.postForEntity(getServerUrl() + BOX_TYPES, httpEntity, Void.class);
 		MultiValueMap<String, Object> secondBox = getBoxTypeCreateAsMultiValueMap("Box 2", "Second Box Description");
-		httpEntity = new HttpEntity<>(secondBox, headers);
-		restTemplate.postForEntity(getServerUrl() + "/box-types", httpEntity, Void.class);
+		httpEntity = createHttpEntity(secondBox);
+		restTemplate.postForEntity(getServerUrl() + BOX_TYPES, httpEntity, Void.class);
 
 		ResponseEntity<List<BoxTypeResponse>> response = restTemplate.exchange(
 				getServerUrl() + "/box-types",
 				HttpMethod.GET,
 				null,
 				new ParameterizedTypeReference<>() {});
-
-		// Проверяем статус ответа и количество боксов
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertNotNull(response.getBody());
 		assertEquals(2, response.getBody().size(), "There should be exactly 2 boxes in the system.");
 	}
 
+	private HttpEntity<MultiValueMap<String, Object>> createHttpEntity(MultiValueMap<String, Object> boxData) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+		return new HttpEntity<>(boxData, headers);
+	}
 
 	private static MultiValueMap<String, Object> getBoxTypeCreateAsMultiValueMap(String name, String description) {
 		MultiValueMap<String, Object> multipartRequest = new LinkedMultiValueMap<>();
 		multipartRequest.add("name", name);
 		multipartRequest.add("description", description);
 		return multipartRequest;
+	}
+
+	private String getServerUrl() {
+		return String.format(SERVER_URL, port);
 	}
 }
