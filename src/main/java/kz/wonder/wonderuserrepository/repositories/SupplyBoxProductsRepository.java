37fca5e694d4fadadc05b23ff9bf206068361569
qqanly <adulatbai@bk.ru>
2024-04-21 21:45:20 +0600

    Refactor OrderServiceImpl and SupplyBoxProductsRepository
    Changed the search logic in OrderServiceImpl and modified the database query in SupplyBoxProductsRepository. Removed specific time check in the processing order block in OrderServiceImpl. Instead of throwing an exception when a product not found in the kaspi store, the code now simply returns. Rewrote the native SQL query in the SupplyBoxProductsRepository to a JPQL query for better readability and maintainability.

--- src/main/java/kz/wonder/wonderuserrepository/repositories/SupplyBoxProductsRepository.java
+++ src/main/java/kz/wonder/wonderuserrepository/repositories/SupplyBoxProductsRepository.java
@@ -9,14 +9,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface SupplyBoxProductsRepository extends JpaRepository<SupplyBoxProduct, Long> {
-    @Query(value = "SELECT Top 1 s FROM SupplyBoxProduct s " +
-            "WHERE s.product.vendorCode = :productVendorCode " +
-            "and s.product.keycloakId = :keycloakId " +
-            "and s.supplyBox.supply.kaspiStore.id = :kaspiStoreId " +
-            "and s.state = :state",
-            nativeQuery = true)
+    @Query("SELECT s FROM SupplyBoxProduct s WHERE s.product.vendorCode = :productVendorCode and s.product.keycloakId = :keycloakId and s.supplyBox.supply.kaspiStore.id = :kaspiStoreId and s.state = :state")
     Optional<SupplyBoxProduct> findFirstByParams(@Param("productVendorCode") String productVendorCode, @Param("keycloakId") String keycloakId, @Param("kaspiStoreId") Long kaspiStoreId, @Param("state") ProductStateInStore state);
-
-
     Optional<SupplyBoxProduct> findByArticle(String productArticle);
+
 }
