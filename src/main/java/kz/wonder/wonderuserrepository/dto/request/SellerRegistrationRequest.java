d8f0c0f2c93b110057403eedd5b980c496c8188f
sembekov <76952248+sembekov@users.noreply.github.com>
2024-04-29 23:47:03 +0500

    Add validation logic to AuthController

--- src/main/java/kz/wonder/wonderuserrepository/dto/request/SellerRegistrationRequest.java
+++ src/main/java/kz/wonder/wonderuserrepository/dto/request/SellerRegistrationRequest.java
@@ -1,6 +1,8 @@
 package kz.wonder.wonderuserrepository.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import kz.wonder.wonderuserrepository.security.keycloak.KeycloakBaseUser;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -9,11 +11,16 @@ import lombok.EqualsAndHashCode;
 @Data
 public class SellerRegistrationRequest extends KeycloakBaseUser {
     // todo: add validation
+    @NotNull(message = "Phone number not found")
+    @Size(min = 10, max = 11)
     private String phoneNumber;
 
     // in kaspi
+    @NotNull
     private String sellerName;
+    @NotNull
     private String sellerId;
+    @NotNull
     private String tokenKaspi;
 
     @JsonIgnore
