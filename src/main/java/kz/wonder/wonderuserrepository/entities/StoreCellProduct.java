c5e4edbf36157c3b33e57d41e4357f423b1ee9ef
qqanly <adulatbai@bk.ru>
2024-05-04 12:45:03 +0500

    Update product processing in orders and supplies
    The commit introduces a StoreCellProductRepository in OrderServiceImpl for providing cell codes in order details. It also adjusts the JoinColumn names in StoreCellProduct entity. Changes are made in SupplyServiceImpl to update product state and accepted time while processing supplies by employees. In SupplyController, the ResponseEntity status is updated to "CREATED" from "OK" after processing supplies. An additional function is provided in StoreCellProductRepository to find by SupplyBoxProductId.

--- src/main/java/kz/wonder/wonderuserrepository/entities/StoreCellProduct.java
+++ src/main/java/kz/wonder/wonderuserrepository/entities/StoreCellProduct.java
@@ -13,11 +13,11 @@ import static kz.wonder.wonderuserrepository.constants.ValueConstants.schemaName
 @Table(name = "store_cell_product", schema = schemaName)
 public class StoreCellProduct extends AbstractEntity<Long> {
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "kaspi_store_id", columnDefinition = "integer", nullable = false)
+    @JoinColumn(name = "store_cell_id", columnDefinition = "integer", nullable = false)
     private StoreCell storeCell;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "supply_product_id", columnDefinition = "integer", nullable = false)
+    @JoinColumn(name = "supply_box_product_id", columnDefinition = "integer", nullable = false)
     private SupplyBoxProduct supplyBoxProduct;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
