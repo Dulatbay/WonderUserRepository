afafeee9b6d60dbf836d5d895e821896322eb9bb
QIT-Projec <Bolashak040206@mail.ru>
2024-05-24 18:02:56 +0500

    Add API documentation for BoxTypesController

--- src/main/java/kz/wonder/wonderuserrepository/controllers/BoxTypesController.java
+++ src/main/java/kz/wonder/wonderuserrepository/controllers/BoxTypesController.java
@@ -1,5 +1,6 @@
 package kz.wonder.wonderuserrepository.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import kz.wonder.wonderuserrepository.dto.request.BoxTypeCreateRequest;
 import kz.wonder.wonderuserrepository.dto.response.BoxTypeResponse;
@@ -20,6 +21,7 @@ public class BoxTypesController {
     private final BoxTypeService boxTypeService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Create new box type", description = "This endpoint allows to create new box types with given name and description")
     public ResponseEntity<Void> create(@ModelAttribute @Valid BoxTypeCreateRequest boxTypeCreateRequest) {
         boxTypeService.createBoxType(boxTypeCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -27,11 +29,13 @@ public class BoxTypesController {
 
 
     @GetMapping()
+    @Operation(summary = "Get all box types of the store", description = "Retrieves all box types of the store. Start by writing id of the required store")
     public ResponseEntity<List<BoxTypeResponse>> getAll(@RequestParam(value = "store-id", required = false) Long storeId) {
         return ResponseEntity.ok(boxTypeService.getAll(storeId));
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Delete existing box type", description = "This endpoint allows to delete existing box type. Fill in the id of the box type")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         boxTypeService.deleteById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
